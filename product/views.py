c7b1ba523665804a5886930ef47d19d6bf9aab80
Prashanth <prashanth.kuchhalapati@gmail.com>
2024-03-01 13:54:48 +0530

    Created Models Viewsets and Superuser

--- product/views.py
+++ product/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    def list(self, request):
+        """
+        A simple viewset for viewing all categories
+
+        """
+
+    queryset = Category.objects.all()
+
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
 
 # Create your views here.
